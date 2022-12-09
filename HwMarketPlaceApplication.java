1a7f9bb78a4cc2a10b57be786675a1643dee77f3
LeoRoA <romanleontev@gmail.com>
2022-12-09 22:08:57 +0700

    Initial commit

---
+++ HwMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwMarketPlaceApplication.class, args);
+    }
+
+}
